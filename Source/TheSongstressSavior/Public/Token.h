f454863f277be50dd8faa6e61286eced785a49a5
Yuvraj Darwani <yuvispace@gmail.com>
2024-06-27 02:42:23 +0530

    added speed token

---
+++ Source/TheSongstressSavior/Public/Token.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Token.generated.h"
+
+class AMelodyCharacter;
+
+UCLASS()
+class THESONGSTRESSSAVIOR_API AToken : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	AToken();
+
+protected:
+	virtual void BeginPlay() override;
+
+public:
+	virtual void Tick(float DeltaTime) override;
+
+	virtual void Interact(AMelodyCharacter* Character);
+};
