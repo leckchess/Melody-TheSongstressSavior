16090b9e115667d878b57cc7cd12eaa435a8813e
Yuvraj Darwani <yuvispace@gmail.com>
2024-07-01 02:00:24 +0530

    added tokens and their blueprint

--- Source/TheSongstressSavior/Private/DisharmonyToken.cpp
+++ Source/TheSongstressSavior/Private/DisharmonyToken.cpp
@@ -7,18 +7,6 @@ void ADisharmonyToken::Interact(AMelodyCharacter* Character)
 {
 	if (Character)
 	{
-
-		// Add logic for disharmony
-
-		
-		// Character->AddStamina(-Character->GetMaxStamina() * 0.15f);
-		// Character->GetCharacterMovement()->MaxWalkSpeed *= 0.85f;
-		// // Display negative effect visual indicator here
-		//
-		// FTimerHandle TimerHandle;
-		// Character->GetWorld()->GetTimerManager().SetTimer(TimerHandle, [Character]() {
-		// 	Character->GetCharacterMovement()->MaxWalkSpeed /= 0.85f;
-		// 	// Remove negative effect visual indicator here
-		// }, 10.0f, false);
+		Character->UseStamina(FMath::RandRange(0.0, 0.3));
 	}
 }
--- Source/TheSongstressSavior/Private/SpeedToken.cpp
+++ Source/TheSongstressSavior/Private/SpeedToken.cpp
@@ -8,15 +8,17 @@ void ASpeedToken::Interact(AMelodyCharacter* Character)
 	if (Character)
 	{
 		// Change the speed here
-		Character->Speed = 50.0f;
+		float curSpeed = Character->Speed;
+		Character->Speed = curSpeed * 3;
 		// Display speed boost visual indicator here
 		// Example logging in BeginPlay
 		UE_LOG(LogTemp, Log, TEXT("Characterss %s initialized."), *GetName());
 
-		// FTimerHandle TimerHandle;
-		// Character->GetWorld()->GetTimerManager().SetTimer(TimerHandle, [Character]() {
-		// 	Character->GetCharacterMovement()->MaxWalkSpeed /= 1.5f;
-		// 	// Remove speed boost visual indicator here
-		// }, 15.0f, false);
+		// Reset after 3 seconds
+		FTimerHandle TimerHandle;
+		Character->GetWorld()->GetTimerManager().SetTimer(TimerHandle, [Character]() {
+			Character->Speed = Character->Speed / 3;
+			// Remove speed boost visual indicator here
+		}, 3.0f, false);
 	}
 }
